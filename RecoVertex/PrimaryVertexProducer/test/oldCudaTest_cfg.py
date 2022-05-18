7220a28d8d5bffd9f2ac12b87363782233c93f8f
Giorgio Pizzati <giorgio.pizzati@gmail.com>
2022-05-18 16:34:56 +0200

    fixed low tracks

--- RecoVertex/PrimaryVertexProducer/test/oldCudaTest_cfg.py
+++ RecoVertex/PrimaryVertexProducer/test/oldCudaTest_cfg.py
@@ -18,7 +18,7 @@ from Configuration.AlCa.GlobalTag import GlobalTag
 process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:phase2_realistic_T21', '')
 process.MessageLogger.cerr.FwkReport.reportEvery = 1
 process.options = cms.untracked.PSet( wantSummary = cms.untracked.bool(True) )
-process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(-1))
+process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(10))
 """
 process.IgProfService = cms.Service("IgProfService",
   reportFirstEvent            = cms.untracked.int32(0),
@@ -37,7 +37,7 @@ fileNames = cms.untracked.vstring(
 #'/store/relval/CMSSW_12_3_0_pre5/RelValTTbar_14TeV/GEN-SIM-RECO/123X_mcRun4_realistic_v4_2026D88noPU-v1/10000/7cbdf153-c397-410e-8af5-dc3905565ced.root'
 ),
 #firstEvent = cms.untracked.uint32(2)
-skipEvents=cms.untracked.uint32(0)
+skipEvents=cms.untracked.uint32(1)
 #skipEvents=cms.untracked.uint32(87)
 )
 process.NVProfilerService = cms.Service("NVProfilerService"
